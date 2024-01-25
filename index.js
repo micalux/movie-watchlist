1307ea887410f664417fd21946978101d93710f9
micalux <micalux@gmail.com>
2024-01-25 11:43:14 +0100

    major update

--- index.js
+++ index.js
@@ -1,19 +1,7 @@
- 
-
-
 const apiKey = '37e37f1f'
 const searchBtn = document.getElementById('search-btn')
 const form = document.querySelector('form')
-
-moviePoster = document.getElementById('movie-poster')
-movieTitle = document.getElementById('movie-title')
-movieRating = document.getElementById('movie-rating')
-movieDuration = document.getElementById('movie-duration')
-movieGenre = document.getElementById('movie-genre')
-movieDescription = document.getElementById('movie-description')
-
-watchlistBtn = document.getElementById('watchlist-btn')
-
+const movie = document.getElementById('movie')
 
 
 form.addEventListener('submit', function(e){
@@ -24,12 +12,42 @@ form.addEventListener('submit', function(e){
         .then (res => res.json())
         .then (data => {
             console.log(data)
-            renderSearch()
+            renderSearch(data)
         })
-    
     form.reset()
 })
 
-const renderSearch = () => {
-    console.log('click')
+const renderSearch = (data) => {
+    movie.innerHTML = `
+        <div class="flex-container">
+            <img src="${data.Poster}" alt="img-title">   
+            <div class="movie-info">
+                <div class="row-1">
+                    <h3>${data.Title}</h3>
+                    <i class="fa-solid fa-star"></i>
+                    <p class="rating">${data.imdbRating}</p>
+                </div>
+                <div class="row-2">
+                    <p class="duration">${data.Runtime}</p>
+                    <p class="genre">${data.Genre}</p>
+                    <div id="watchlist-btn">
+                        <i class="fa-solid fa-circle-plus"></i>
+                        <p class="watchlist">Watchlist</p>
+                    </div>
+                </div>              
+                <p class="description">${data.Plot}</p>
+            </div>
+         </div> 
+         <div class="separator"></div> 
+    `
+    const watchlistBtn = document.getElementById('watchlist-btn')
+    addToWhatchlist(watchlistBtn)
+
 }
+
+function addToWhatchlist(e) {
+    e.addEventListener('click', () => {
+        console.log('click')
+    })
+}
+
